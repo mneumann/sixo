2e36a0eac625b7aa7904b7871c8b6dd01ef18368
tuberkel <tuberkel>
2006-03-20 22:21:38 +0000

    Initial revision

---
+++ sixo20/sw/statdev.c
@@ -0,0 +1,86 @@
+/***********************************************************************
+ *
+ *  CVS-Filename:   $RCSfile$
+ *  Version:        $Revision$
+ *  Last Changed:   $Date$
+ *  Changed by:     $Author$
+ *
+ *  --------------------------------------------------------------------
+ *
+ *  Created:        2006-01-04 by Ralf Krizsan
+ *  Project:        SIxO
+ *  Module:         Statistic Device
+ *  Purpose:        handles the statistic device screen
+ *  Comments:       -
+ *
+ *  --------------------------------------------------------------------
+ *
+ *  Compiler:       Renesas NC30WA V.5.00 Release 2
+ *  Options:        -
+ *
+ *  --------------------------------------------------------------------
+ *
+ *  Licence details
+ *
+ *  This software is copyright © 2001-2004 by N&K Development, Germany
+ *
+ *  You can redistribute it and/or modify it under the terms of the
+ *  GNU General Public License version 2 as published by the
+ *  Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+ *  MA  02111-1307, USA.
+ *
+ *  To purchase support or enhancements for this software, contact:
+ *
+ *      N&K Development                     N&K Development
+ *      Arnold Neugebauer                   Ralf Krizsan
+ *      Stöckener Str. 115                  Knickstr. 10
+ *      30419 Hannover                      30890 Barsinghausen
+ *      arnold.neugebauer@web.de            ralf.krizsan@web.de
+ *
+ *      or try http://www.sixo.de
+ *
+ *  --------------------------------------------------------------------
+ *
+ *  Disclaimer of warrenty
+ *
+ *  The software is provided as is without warranties of
+ *  any kind, either express or implied. To the fullest
+ *  extent allowed by applicable law, N&K Development
+ *  disclaims all warranties, express or implied,
+ *  including, but not limited to, implied warranties of
+ *  merchantability, fitness for a particular purpose, and
+ *  non-infringement.
+ *
+ *  --------------------------------------------------------------------
+ *
+ *  CVS History
+ *
+ *  This information is automatically added while 'commiting' the
+ *  changes to CVC ('Log message'):
+ *
+ * $Log$
+ * Revision 1.1  2006/03/20 22:21:38  tuberkel
+ * Initial revision
+ *
+ * Revision 1.1  2006/02/18 15:20:23  Ralf
+ * - BIKE_MOTOBAU: statistics and lapcounter device added
+ *
+ *
+ ************************************************************************ */
+
+
+// this is a special device created for Motobau / Berlin
+// and will not be visible in standard version
+
+
+
+
